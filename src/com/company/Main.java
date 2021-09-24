6ead6bfb21b726020fb1f2773e21bb9127939453
Nicolai <s203952@dtu.dk>
2021-09-24 15:36:12 +0200

    First Iteration of pyramid printer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,9 @@ import java.util.Random;
 
 
 public class Main {
+    int size;
+    int LOG = 0;
     static Scanner input = new Scanner(System.in);
-
     // this class stores the values of the different parts of the pyramid
     public Main(int size) {
         String fd;
@@ -71,11 +72,70 @@ public class Main {
             break;
         default:
             System.out.println("invalid size");
+            break;
     }
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int size = input.nextInt();
-        Main obj = new Main(size);
+        int x = 1;
+        int size;
+        double balance = 100;
+        String answer;
+        String answer2;
+        int price;
+        int earnings;
+        while(x == 1) {
+            System.out.println("do you want to build a new pyramid?");
+            answer = input.nextLine();
+            if(answer.equals("y")) {
+                System.out.println("insert the size of your new pyramid");
+                size = input.nextInt();
+                price = size * 20;
+                System.out.println("this pyramid costs: " + price + "$");
+                try {
+                    TimeUnit.SECONDS.sleep(4);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                price = size * 20;
+                balance = balance - price;
+                System.out.println("you have bought a pyramid of size: " + size + ". Your new balance is: " + balance + "$");
+                Main obj = new Main(size);
+                try {
+                    TimeUnit.SECONDS.sleep(4);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                earnings = size * 10;
+                balance = earnings + balance;
+                System.out.println("you have earned: " + earnings + "$" + "from rent");
+                System.out.println("you now have a balance of: " + balance + "$");
+                try {
+                    TimeUnit.SECONDS.sleep(4);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if (answer.equals("n")) {
+                earnings = 0;
+                System.out.println("nothing was done this turn");
+                System.out.println("you have earned: " + earnings + "$");
+                System.out.println("you now have a balance of: " + balance + "$");
+                try {
+                    TimeUnit.SECONDS.sleep(4);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                    System.out.println("nothing was done this turn");
+                    try {
+                        TimeUnit.SECONDS.sleep(4);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+        }
     }
 }
